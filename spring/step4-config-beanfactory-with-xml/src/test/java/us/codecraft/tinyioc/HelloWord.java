0d7c2a838cd5d5ed1028ca31ac843418df09c7f9
dengliwen <983546945@qq.com>
2019-09-10 10:12:34 +0800

    all

---
+++ spring/step4-config-beanfactory-with-xml/src/test/java/us/codecraft/tinyioc/HelloWord.java
@@ -0,0 +1,11 @@
+package us.codecraft.tinyioc;
+
+/**
+ * @author dengliwen
+ * @date 2019/8/18
+ * @desc
+ */
+public interface HelloWord {
+
+    void helloWorld();
+}
---
+++ springboot-es/src/main/java/com/dlw/study/metadata/Collector.java
@@ -0,0 +1,15 @@
+package com.dlw.study.metadata;
+
+import com.dlw.study.common.R;
+import org.elasticsearch.action.search.SearchResponse;
+
+/**
+ * @author dengliwen
+ * @date 2019/9/5
+ * @desc
+ */
+public interface Collector {
+
+    R collect();
+
+}
